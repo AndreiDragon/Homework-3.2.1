152645b0c65dc1416fedf30ad6d2381e29a4f7ef
AndreiDragon <Andrei-the-hot@yandex.ru>
2021-12-13 20:05:17 +0400

    Keep 'em coming CJ!

---
+++ Bonus Miles/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticket_price = 40000;
+        long bonus_miles = ticket_price / 20;
+        System.out.println(bonus_miles);
+    }
+}
